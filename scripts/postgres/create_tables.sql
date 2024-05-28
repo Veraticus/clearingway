b4ac596ac766db946b4796d0fef1f664a24c0f7d
swood456 <smwood456@gmail.com>
2024-05-27 23:19:09 -0400

    Seed initial DB with table and some data

--- scripts/postgres/create_tables.sql
+++ scripts/postgres/create_tables.sql
@@ -1,7 +1,11 @@
+BEGIN;
+
 -- Create Users table
 CREATE TABLE IF NOT EXISTS users (
-    discordid TEXT NOT NULL PRIMARY KEY
-    world TEXT NOT NULL
-    firstname TEXT NOT NULL
+    discordid TEXT NOT NULL PRIMARY KEY,
+    world TEXT NOT NULL,
+    firstname TEXT NOT NULL,
     lastname TEXT NOT NULL
-)
+);
+
+COMMIT;
--- scripts/postgres/seed_data.sql
+++ scripts/postgres/seed_data.sql
@@ -1,3 +1,8 @@
+BEGIN;
+
+-- Example of how to seed some user data
 INSERT INTO users (discordid, world, firstname, lastname)
 VALUES
-("TestDiscordUser", "Leviathan", "Test", "User")
+('TestDiscordUser', 'Leviathan', 'Test', 'User');
+
+COMMIT;

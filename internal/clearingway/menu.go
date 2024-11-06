40931501089074d08aba07db22b7b1c59e225902
OTCompa <41562938+OTCompa@users.noreply.github.com>
2024-11-05 19:06:34 -0600

    Add buttons to remove categories of roles (#40)
    * update menu description for NAUR
    
    * add remove buttons for each encounter category
    
    * further update menu description for NAUR
    
    * formatting adjustment for NAUR menu
    
    * add NB tag

--- internal/clearingway/menu.go
+++ internal/clearingway/menu.go
@@ -26,6 +26,7 @@ const (
 	CommandRemoveComfy      CommandType = "removeComfy"
 	CommandRemoveColor      CommandType = "removeColor"
 	CommandRemoveAll        CommandType = "removeAll"
+	CommandRemoveEncounter  CommandType = "removeEncounter"
 	CommandEncounterProcess CommandType = "encounterProcess"
 )
 
@@ -167,6 +168,28 @@ func (m *Menu) MenuRemoveInit() {
 		},
 	}
 
+}
+
+func (m *Menu) MenuRemoveAddButton(menuEncounterToAdd *Menu) {
+	// create button
+	customIDslice := []string{string(MenuRemove), string(CommandRemoveEncounter), string(menuEncounterToAdd.Name)}
+	button := discordgo.Button{
+		Label:    "Remove " + menuEncounterToAdd.Title,
+		Style:    discordgo.DangerButton,
+		Disabled: false,
+		CustomID: strings.Join(customIDslice, " "),
+	}
+
+	// create new ActionsRow component if already made, append to it
+	buttonRows := &m.AdditionalData.MessageEphemeral.Data.Components
+	if len(*buttonRows) < 2 {
+		*buttonRows = append(*buttonRows, discordgo.ActionsRow{Components: []discordgo.MessageComponent{button}})
+	} else {
+		// creates a copy of ActionsRow, appends the button, then sets the element to the appended copy
+		addButtonRow := (*buttonRows)[1].(discordgo.ActionsRow)
+		addButtonRow.Components = append(addButtonRow.Components, button)
+		(*buttonRows)[1] = addButtonRow
+	}
 }
 
 func (g *Guild) DefaultMenus() {
